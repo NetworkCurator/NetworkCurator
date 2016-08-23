f3180ef94bfc5bfbd5d9696871c55db3cf810899
tkonopka <tkonopka@users.noreply.github.com>
2016-08-24 00:18:58 +0200

    allow for more local definitions (e.g. path)

--- nc-admin/install/install-settings.php
+++ nc-admin/install/install-settings.php
@@ -2,7 +2,9 @@
 
 // This file contains settings using during the NetworkCurator installation
 // Connection to SQL server
-define("SERVER", "localhost");
+if (!defined("SERVER")) {
+    define("SERVER", "localhost");
+}
 
 
 // Root user and password with full access
@@ -11,44 +13,60 @@ define("SERVER", "localhost");
 // e.g. in phpmyadmin on the "Privileges" page
 //
 // !! Change password in local copy !! 
+// !! Create a file install-settings-local.php and add similar defs with new passwords
 //
-define("DB_ROOT", "nc_admin0");
+if (!defined("DB_ROOT")) {
+    define("DB_ROOT", "nc_admin0");
+}
 if (!defined("DB_ROOT_PASSWD")) {
     define("DB_ROOT_PASSWD", "yellowflowersontheroadside");
 }
 
 
 // Name of new database
-define("DB_NAME", "networkcurator");
-
+if (!defined("DB_NAME")) {
+    define("DB_NAME", "networkcurator");
+}
 
 // name and password for database administrator
 // This user must be created in the database prior to installation
 //
 // !! Change password in local copy !!
 //
-define("DB_ADMIN", "nc_admin1");
+if (!defined("DB_ADMIN")) {
+    define("DB_ADMIN", "nc_admin1");
+}
 if (!defined("DB_ADMIN_PASSWD")) {
     define("DB_ADMIN_PASSWD", "blueskieswithcloudstoday");
 }
 
 
 // prefix for tables
-define("DB_TABLE_PREFIX", "nc");
+if (!defined("DB_TABLE_PREFIX")) {
+    define("DB_TABLE_PREFIX", "nc");
+}
 
 
 // settings for the web server - position of NetworkCurator on a webpage
-define("NC_PATH", "/NetworkCurator");
+if (!defined("NC_PATH")) {
+    define("NC_PATH", "/NetworkCurator");
+}
 
 
 // name of NetworkCurator instance (api id)
-define("NC_APP_ID", "myncname");
+if (!defined("NC_APP_ID")) {
+    define("NC_APP_ID", "myncname");
+}
 
 
 // name of website name, e.g. NetworkCurator
-define("NC_SITE_NAME", "NetworkCurator");
+if (!defined("NC_SITE_NAME")) {
+    define("NC_SITE_NAME", "NetworkCurator");
+}
 
 
 // length of random strings in ids
-define("NC_ID_LEN", 9);
+if (!defined("NC_ID_LEN")) {
+    define("NC_ID_LEN", 9);
+}
 ?>
