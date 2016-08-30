61f1526e34ff0287141efcf2a13ecbbac5c4d5eb
tkonopka <tkonopka@users.noreply.github.com>
2016-08-30 19:16:14 +0200

    adjusted gitignore to allow nc-constants.php

---
+++ nc-admin/config/nc-constants.php
@@ -0,0 +1,25 @@
+<?php
+
+// Definitions for constants used in annotations/values tables
+define('NC_NAME',	0);
+define('NC_TITLE',	1);
+define('NC_ABSTRACT',	2);
+define('NC_CONTENT',	3);
+define('NC_UNIT',	10);
+define('NC_COMMENT',	20);
+define('NC_SUBCOMMENT',	21);
+
+// Definitions for constants for status
+define('NC_ACTIVE',	1);
+define('NC_DEPRECATED',	-1);
+define('NC_OLD',	0);
+
+// Definitions for permission level
+define('NC_PERM_NONE',	0);
+define('NC_PERM_VIEW',	1);
+define('NC_PERM_COMMENT',2);
+define('NC_PERM_EDIT',	3);
+define('NC_PERM_CURATE',4);
+define('NC_PERM_SUPER',	9);
+
+?>
