5ae6f3c75556c5ef2c3603f3d2cfa23e5b4bde43
tkonopka <tkonopka@users.noreply.github.com>
2016-11-04 14:46:47 +0100

    avoid self encryption

--- nc-admin/tools/try.php
+++ nc-admin/tools/try.php
@@ -1,9 +1,15 @@
 <?php
 
-$params = ["AB"=>4, "BC"=>0];
+$params = ["AB"=>4, "BC"=>0, "XYZ"=>"hello hello"];
 print_r($params);
 
 $bb = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, "a72930bd82ef001f", json_encode($params), MCRYPT_MODE_ECB));
 echo $bb. "\n";
+
+$pjson = json_encode($params);
+$bjson = json_encode($bb);
+
+echo $pjson ."\n";
+echo $bjson . "\n";
 
 ?>
