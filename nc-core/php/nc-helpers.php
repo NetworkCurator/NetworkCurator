7116335c9d2ac1709f909a890c9c301f700c6b24
tkonopka <tkonopka@users.noreply.github.com>
2017-03-21 10:56:45 +0000

    more support for local files (navbar, header, footer, etc)

--- nc-core/php/nc-helpers.php
+++ nc-core/php/nc-helpers.php
@@ -4,9 +4,6 @@
  * Helper functions to deal with node and link class structure
  */
 
-
-
-
 /**
  * 
  * @param string $js - name of js object to assign to (e.g. nc.md)
@@ -16,8 +13,29 @@
  * 
  * a string with a <script> tag encoding the array into js
  */
-function ncScriptObject($js, $arr) {      
-    return "<script>$js = ".  json_encode($arr)."; </script>";
+function ncScriptObject($js, $arr) {
+    return "<script>$js = " . json_encode($arr) . "; </script>";
+}
+
+
+/**
+ * check if a local version of a file exists.
+ * 
+ * e.g. input $path = "file.php"
+ * checks if file-local.php exists. 
+ * If yes, returns file-local.php.
+ * If no, returns file.php
+ *  
+ * @param type $path
+ */
+function ncGetLocalFile($path) {    
+    $pathend = substr($path, -4);
+    $localfile = substr($path, 0, -4) . "-local" . $pathend;  
+    if (file_exists($localfile)) {        
+        return $localfile;        
+    } else {        
+        return $path;        
+    }    
 }
 
 ?>
